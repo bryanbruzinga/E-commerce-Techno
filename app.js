f339f6dadbc306049597f7b3c93e0222c7a54220
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-01-28 19:44:55 -0300

    Adicionado modal de produto adicionado ao carrinho

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const vm = new Vue({
         produtos: [],
         produto: false,
         carrinho: [],
+        mensagemAlerta: "Item adicionado",
+        alertaAtivo: false
     },
     filters: {
         numeroPreco(valor) {
@@ -50,7 +52,8 @@ const vm = new Vue({
         adicionarItem() {
             this.produto.estoque--;
             const {id, nome, preco} = this.produto;
-            this.carrinho.push({id, nome, preco})
+            this.carrinho.push({id, nome, preco});
+            this.alerta(`${nome} adicionado ao carrinho.`);
         },
         removerItem(index) {
             this.carrinho.splice(index, 1)
@@ -59,6 +62,13 @@ const vm = new Vue({
             if (window.localStorage.carrinho) {
                 this.carrinho = JSON.parse(window.localStorage.carrinho)
             }
+        },
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false
+            }, 1500);
         }
     },
     watch: {
