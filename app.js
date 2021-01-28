b8483bc2d9718e4940f689e36b03cca870a7c37f
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-01-28 11:53:10 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        produtos: []
+    },
+    methods: {
+        fetchProdutos() {
+            fetch('./api/produtos.json')
+            .then(r => r.json())
+            .then(json => {
+                this.produtos = json;
+            })
+        }
+    },
+    created() {
+        this.fetchProdutos();
+    }
+})
